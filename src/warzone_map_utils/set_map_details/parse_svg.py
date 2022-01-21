c560dba66ec05dd998255ae20620b90d96edc023
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-01-20 18:37:52 -0800

    enable adding bonuses by parsing a map svg file

--- src/warzone_map_utils/set_map_details/parse_svg.py
+++ src/warzone_map_utils/set_map_details/parse_svg.py
@@ -12,23 +12,29 @@ def get_uri(key: str) -> str:
 
 
 def get_set_map_details_commands(map_path: str) -> List[types.Command]:
+    layers = get_layers(map_path)
 
+    commands = (
+        get_set_territory_name_commands(layers[svg.TERRITORIES_LAYER])
+        + get_add_bonus_commands(layers[svg.BONUS_LINKS_LAYER], layers[svg.METADATA_LAYER])
+    )
+    return commands
+
+
+def get_layers(map_path):
     map_xml = ET.parse(map_path)
     root = map_xml.getroot()
     layers: Dict[str, ET.Element] = {
         node.get(get_uri(svg.LABEL_ATTRIBUTE)): node
         for node in root.findall(f"./*[@{svg.LABEL_ATTRIBUTE}]", svg.NAMESPACES)
     }
-
-    commands = []
-    commands += get_set_territory_name_commands(layers[svg.TERRITORIES_LAYER])
-    return commands
+    return layers
 
 
 def get_set_territory_name_commands(territory_layer_node: ET.Element) -> List[types.Command]:
     territory_nodes = [
         node for node in territory_layer_node.findall(f"./{svg.PATH_TAG}", svg.NAMESPACES)
-        if svg.TERRITORY_IDENTIFIER in node.get('id')
+        if svg.TERRITORY_IDENTIFIER in node.get(svg.ID_ATTRIBUTE)
     ]
 
     commands = [
@@ -55,30 +61,45 @@ def get_set_territory_name_command(territory_node: ET.Element) -> types.Command:
 
 
 def get_add_bonus_commands(
-        bonus_link_layer_node: Element, metadata_layer_node: Element
+        bonus_link_layer_node: ET.Element, metadata_layer_node: ET.Element
 ) -> List[types.Command]:
     bonus_link_nodes = {
-        node.getAttribute(svg.ID_ATTRIBUTE):
-        node for node in bonus_link_layer_node.getElementsByTagName(svg.PATH_TAG)
-        if svg.BONUS_LINK_IDENTIFIER in node.getAttribute(svg.ID_ATTRIBUTE)
+        node.get(svg.ID_ATTRIBUTE):
+        node for node in bonus_link_layer_node.findall(f"./{svg.PATH_TAG}", svg.NAMESPACES)
+        if svg.BONUS_LINK_IDENTIFIER in node.get(svg.ID_ATTRIBUTE)
     }
-    bonus_layer_nodes = [
-        node for node in metadata_layer_node.childNodes
-        if node.getAttribute(svg.LABEL_ATTRIBUTE) == svg.BONUSES_LAYER
-    ][0].getElementsByTagName(svg.GROUP_TAG)
-    # todo create commands
-    for node in bonus_layer_nodes:
-        # todo get bonus name
-        bonus_name = ''
-        # todo get bonus value
-        bonus_value = 0
-        # todo find bonus link node if exists to get color
-        bonus_link_node = bonus_link_nodes[get_bonus_link_id(bonus_name)]
-        # todo create addBonus command
-        pass
+    bonus_layer_nodes = (
+        metadata_layer_node.findall(
+            f"./{svg.GROUP_TAG}[@{svg.LABEL_ATTRIBUTE}='{svg.BONUSES_LAYER}']"
+            f"//{svg.GROUP_TAG}[@{svg.LABEL_ATTRIBUTE}]", svg.NAMESPACES)
+    )
 
-    commands = []
+    commands = [get_add_bonus_command(node, bonus_link_nodes) for node in bonus_layer_nodes]
     return commands
+
+
+def get_add_bonus_command(
+        node: ET.Element, bonus_link_nodes: dict[str, ET.Element]
+) -> types.Command:
+    bonus_name, bonus_value = node.get(get_uri(svg.LABEL_ATTRIBUTE)).split(': ')
+
+    if bonus_link_node := bonus_link_nodes.get(get_bonus_link_id(bonus_name)):
+        node_style = {
+            key: value for key, value in (
+                field.split(':') for field in bonus_link_node.get('style').split(';')
+            )
+        }
+        bonus_color = node_style['fill'].upper()
+    else:
+        bonus_color = '#000000'
+
+    command = {
+        'command': 'addBonus',
+        'name': bonus_name,
+        'armies': bonus_value,
+        'color': bonus_color
+    }
+    return command
 
 
 def get_bonus_link_id(bonus_name: str) -> str:
